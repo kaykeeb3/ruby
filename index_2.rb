86da3120e50d657a758b35e885f6d6e6f625bd69
Kayke Barbosa <104206837+Kayke-Ti@users.noreply.github.com>
2023-09-01 13:29:02 -0300

    add new commit index_2

---
+++ index_2.rb
@@ -0,0 +1,18 @@
+
+# Definindo uma variável inteira
+idade = 30
+
+# Definindo uma variável de ponto flutuante
+altura = 1.75
+
+# Definindo uma variável de string
+nome = "João"
+
+# Definindo uma variável booleana
+tem_carro = true
+
+# Imprimindo os valores das variáveis
+puts "Nome: #{nome}"
+puts "Idade: #{idade} anos"
+puts "Altura: #{altura} metros"
+puts "Tem carro? #{tem_carro ? 'Sim' : 'Não'}"
