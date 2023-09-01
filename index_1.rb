c85fcf2814db7d1cee39f2cae6450e2bf029072c
Kayke Barbosa <104206837+Kayke-Ti@users.noreply.github.com>
2023-09-01 13:28:22 -0300

    add new commit index_1

---
+++ index_1.rb
@@ -0,0 +1,20 @@
+# Operador lógico "E" (AND)
+valor1 = true
+valor2 = false
+
+resultado_e = valor1 && valor2
+puts "Operador 'E' (AND): #{valor1} && #{valor2} = #{resultado_e}"
+
+# Operador lógico "OU" (OR)
+valor1 = true
+valor2 = false
+
+resultado_ou = valor1 || valor2
+puts "Operador 'OU' (OR): #{valor1} || #{valor2} = #{resultado_ou}"
+
+# Operador lógico "NÃO" (NOT)
+valor = true
+
+resultado_nao = !valor
+puts "Operador 'NÃO' (NOT): !#{valor} = #{resultado_nao}"
+
